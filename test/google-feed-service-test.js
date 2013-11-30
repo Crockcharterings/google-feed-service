c71b0a8a2942765271c732ece2dbfb9d20759bb6
tomkp <tom@tomkp.com>
2013-11-30 18:33:15 +0000

    ...

--- test/google-feed-service-test.js
+++ test/google-feed-service-test.js
@@ -5,16 +5,11 @@ var service = require('../lib/google-feed-service.js');
 describe('Google Feed Service', function () {
 
     it('feed url should match', function (done) {
-        $.when(
-            service('http://feeds.feedburner.com/CssTricks')
-        ).then(
-            function (data) {
-                console.info('success', data);
-                assert.equal('http://feeds.feedburner.com/CssTricks', data.responseData.feed.feedUrl);
-                done();
-            }
-        );
-
+        service('http://feeds.feedburner.com/CssTricks', function (data) {
+            console.info('success', data);
+            assert.equal('http://feeds.feedburner.com/CssTricks', data.responseData.feed.feedUrl);
+            done();
+        });
     });
 
 });
